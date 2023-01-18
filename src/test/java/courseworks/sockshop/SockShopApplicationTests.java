48866475aaf0bb418cb3bf599519713b833c31ee
BULAT <ibragimovbi@yandex.ru>
2023-01-18 17:09:19 +0300

    Initial commit

---
+++ src/test/java/courseworks/sockshop/SockShopApplicationTests.java
@@ -0,0 +1,13 @@
+package courseworks.sockshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
